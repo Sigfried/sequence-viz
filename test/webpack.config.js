fd58f204385ef6397d04f1c5db4aa31b9c1a7738
Sigfried Gold <sigfried@sigfried.org>
2015-06-26 07:01:14 -0400

    no idea what's going on

---
+++ test/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: './sequence-viz-spec.js',
+    output: {
+        path: './',
+        filename: './bundle.js',
+    },
+    resolve: {
+        alias: { moment: 'moment/moment.js'},
+        modulesDirectories: ['../node_modules', '../bower_components'],
+    },
+    //devtool:"eval",
+    devtool: "source-map",
+    //watch: true,
+    colors: true,
+    progress: true,
+    cache: true,
+    debug: true
+};
