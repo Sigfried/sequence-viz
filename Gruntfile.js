29037209610e7fd63ef4b5d0268d5dc8fe7a142e
Sigfried Gold <sigfried@sigfried.org>
2015-06-26 06:03:35 -0400

    changes from 1.4 years ago. first committing just build stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,31 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
-    watch: {
-      files: [ "./*.js","./test/*.js", "./README.md"],
-      karma: {
-            //files: [ "*.js","test/*.js"],
-            files: [
-                // TEMPORARY LOCATIONS!!!!
-                '../underscore-unchained/bower_components/underscore/underscore.js',
-                '../underscore-unchained/bower_components/1670507/underscoreAddon.js',
-                '../underscore-unchained/underscore-unchained.js',
-                '../supergroup/supergroup.js',
-                'node_modules/d3/d3.js',
-                //'../bower_components/underscore/underscore.js',
-                //'../bower_components/1670507/underscoreAddon.js',
-                //'../bower_components/underscore-unchained/underscore-unchained.js',
-                //'../supergroup.js',
-                'bower_components/momentjs/moment.js',
-                'evtData.js',
-                'lifeflowData.js',
-                'test/*.js'
-                //, {pattern: './hurricane.csv', included: false}
-                //, {pattern: './base/hurricane.csv', included: false}
-            ],
-            //verbose: true,
-            tasks: ['karma:unit:run'] //NOTE the :run flag
-      },
-      tasks: [ ]
-      //tasks: [ "groc" ]
+    browserify: {
+      "./bundle.js": [ "./sequence-viz.js" ],
+      "./test/bundle.js": [ "./test/sequence-viz-spec.js" ]
+      , options: { 
+            //transform: ["debowerify", "decomponentify", "deamdify", "deglobalify"],
+      }
     },
+    watch: {
+      files: [ "./*.js", "./test/*spec.js","./README.md"],
+      tasks: [ "browserify" ]
+    },
+  /*
     groc: {
-        javascript: [ "./supergroup.js", "README.md" ],
+        javascript: [ "./supergroup.js", "./test/supergroup.spec.js","./README.md"],
         options: { "out": "doc/" }
     },
     jshint: {
-        all: ["Gruntfile.js", "./supergroup.js", "test/**/*.js"]
+        all: ["Gruntfile.js", "./supergroup.js", "test/** /*.js"]
         , options: { laxcomma: true }
-    },
-    karma: {
-        unit: {
-            configFile: './test/karma.conf.js',
-            browsers: ['PhantomJS'],
-            background: true
-        }
-        //basePath: "test",
     }
+*/
   });
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-karma");
   grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-groc");
-  grunt.registerTask("default", ["karma"]);
+  grunt.registerTask("default", ["browserify" /*, "jshint", "groc" */]);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,12 @@ module.exports = {
         alias: { moment: 'moment/moment.js'},
         modulesDirectories: ['node_modules', 'bower_components'],
     },
+    plugins: ['concat'],
+    //devtool:"eval",
+    devtool: "source-map",
     //watch: true,
     colors: true,
     progress: true,
-    cache: true,
+    cache: false,
     debug: true
 };
